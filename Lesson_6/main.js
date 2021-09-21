55575e28e41da50516929443cd94dc7a6c71e6de
MashinaSSokom <skltan@mail.ru>
2021-09-21 23:58:15 +0800

    add task_2

--- Lesson_6/main.js
+++ Lesson_6/main.js
@@ -43,17 +43,23 @@ function task_2() {
     let basket_tittle = document.createElement('h1')
     basket_tittle.textContent = 'Корзина'
     basket.before(basket_tittle)
+    let total_price = document.createElement('h3')
+    total_price.className = 'total-price'
+    total_price.textContent = 'К оплате: 0 руб'
+    basket.append(total_price)
+    let basket_products_list = document.createElement('h4')
+    basket_products_list.className = 'basket-products-list'
+    basket_products_list.textContent = 'Список покупок:'
+    basket.append(basket_products_list)
+
+    // for (let i = 0; i < products_list.length; i++)
+
 
     let products = document.querySelector('.products')
     products.style.cssText = `
         display: flex;
         flex-wrap: wrap;
-
     `
-
-
-
-
     let products_tittle = document.createElement('h1')
     products_tittle.textContent = 'Каталог'
     products.before(products_tittle)
@@ -87,13 +93,59 @@ function task_2() {
         btn_buy.textContent = 'Купить'
         btn_buy.style.alignSelf = 'flex-end'
         btn_buy.onclick = function () {
-            basket_list.push(products_list[i])
-            console.log(basket_list)
-        }
 
+            let products_in_basket = document.querySelectorAll('.product-in-basket')
+            products_in_basket.forEach(e=>e.remove())
+            basket_list.push(products_list[i])
+            let count = 0
+            for (let i = 0; i < basket_list.length; i++) {
+                count += +basket_list[i].price
+            }
+            total_price = document.querySelector('.total-price')
+            total_price.textContent = `К оплате: ${count} руб.`
+
+            renderBasket(basket_list)
+        }
 
 
         product.append(tittle, description, price, img, btn_buy)
         products.append(product)
+    }
+
+    let renderBasket = function (basket_list){
+        let basket_list_to_render = []
+        basket_list.forEach((el, i, arr) => {
+            if ( basket_list_to_render.indexOf(el)!== -1 ){
+                let idx = basket_list_to_render.findIndex(e => e===el)
+                basket_list_to_render[idx]['count'] += 1
+            } else {
+                el['count'] = 1
+                basket_list_to_render.push(el)
+            }
+        })
+        basket_list_to_render.forEach((el, i, arr) => {
+            let product_in_basket = document.createElement('div')
+            product_in_basket.className = 'product-in-basket'
+            product_in_basket.style.cssText = `padding: 5px;
+            width: 400px;
+            margin: 3px;
+            border: 1px solid black;
+            display: flex;
+            flex-direction: column;
+            justify-content: left;
+        `
+            let tittle = document.createElement('p')
+            tittle.textContent = `Название: ${el.name}`
+            tittle.style.margin = '0'
+            let count = document.createElement('p')
+            count.textContent = `Количество: ${el.count}`
+            count.style.margin = '0'
+            let price = document.createElement('p')
+            price.textContent = `Стоимость: ${+el.price * el.count}`
+            price.style.margin = '0'
+
+            product_in_basket.append(tittle, count, price)
+            basket.append(product_in_basket)
+        })
     }
 }
